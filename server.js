fe3dde074ff079f485c04ef77e35381adba1697c
charin <charin.aewzipo@gmail.com>
2022-08-14 01:19:09 +0700

    make better

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const compression = require("compression");
 const path = require("path");
 const cors = require("cors");
 const app = express();
-const cors = require("cors");
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-app.options("*", cors());
+
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use(compression());
 app.use(express.static(path.join(__dirname, "build")));
 app.get("*", (req, res) => {
