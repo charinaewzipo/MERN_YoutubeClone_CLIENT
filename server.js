30841b91359bea8a25741817428a933530b51822
charin <charin.aewzipo@gmail.com>
2022-08-14 00:20:03 +0700

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const compression = require("compression");
 const path = require("path");
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 app.use(compression());
 app.use(express.static(path.join(__dirname, "build")));
 app.get("*", (req, res) => {
